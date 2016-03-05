a7803c60bef53346984cd8c96c7f103ee4607143
euetova <euetova@gmail.com>
2016-03-05 15:55:45 +0400

    made folders

--- www-copy/www-copy/poetic-beta.js
+++
@@ -1,5 +0,0 @@
-settings.linksName = false;
-settings.disableTree = true;
-settings.disableSecondTable = true;
-settings.flagsName = "n-reqflags-poetic.html";
-settings.mode="poetic";
--- www-copy/www-copy/poetic.js
+++
@@ -1,6 +0,0 @@
-settings.linksName = false;
-settings.disableTree = true;
-settings.disableSecondTable = true;
-settings.flagsName = "reqflags-poetic.html";
-settings.mode="poetic";
-settings.customDraw = true;
